96a727ea0a3243c26d9a51f5c83ca507bf07f0f5
@Josue <62188147+jshkey@users.noreply.github.com>
2020-11-27 09:03:30 -0600

    Agregue tipos.kt y boolean.kt

---
+++ Tipos.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    //numeros
+    val int : Int = 123
+    var num2 : Float = int.toFloat()
+    val long = 1234567L
+    val double = 12.34
+    val float = 12.34F
+    val hexadecimal = 0xAB
+    val binary = 0b01010101
+
+    //println("\nLos datos son Int $int, Long $long, Double $double, Float $float, Hexadecimal $hexadecimal y Binario $binary.")
+    println(int)
+    println(num2)
+
+}
--- variables.kt
+++ variables.kt
@@ -1,16 +1,15 @@
 fun main()
 {
     var nombre = "Josue"
-    //nombre = "Gerardo" /* Muestra el segundo valor dado que se esta reasignando la variables asi que muestra el nuevo valor */
+    nombre = "Gerardo"
 
     println("Mi nombre es: $nombre")
 
     val apellido = "Morales"
-    //apellido = "Castillo" /* Aqui muestra el primer valor porque el val no permite la reasignacion asi que toma el primer valor como el unico valido */
+    //apellido = "Castillo"
 
     println("Mi apellido es: $apellido")
 
     var edad = "19"
-
     println("Mi nombre es $nombre $apellido y tengo $edad años")
 }
